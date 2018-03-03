a939d75904a215ee3613105360f3beb587133175
zhaoxingniu <1436179103@qq.com>
2018-03-03 23:09:21 +0800

    finish

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,14 +40,23 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J=J+sum(sum((R.*(X*Theta'-Y).^2)))/2+lambda/2*sum(sum(Theta.^2))+...  
+    lambda/2*sum(sum(X.^2)); 
 
-
-
-
-
-
-
-
+% X grad 
+for i=1:size(X,1)  
+    idx = find(R(i,:)==1);  
+    Thetatemp = Theta(idx,:);  
+    Ytemp = Y(i,idx);  
+    X_grad(i,:)=(X(i,:)*Thetatemp'-Ytemp)*Thetatemp+lambda*X(i,:);  
+end  
+% grad grad
+for j=1:size(Theta,1)  
+    idx = find(R(:,j)==1);  
+    Xtemp = X(idx,:);  
+    Ytemp = Y(idx,j);  
+   Theta_grad(j,:)=(Xtemp*Theta(j,:)'-Ytemp)'*Xtemp+lambda*Theta(j,:);  
+end  
 
 
 
